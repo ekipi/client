c22066a08b3ced2cafaaf0e49ec2b28a8e3c0424
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-10-01 00:44:31 +0530

    changed the api url for prod

--- ekipi-client/src/app/shared/shared.constants.ts
+++ ekipi-client/src/app/shared/shared.constants.ts
@@ -1,4 +1,6 @@
 export const SERVER = {
-    URL : 'http://127.0.0.1:4201/',
-    API_URL : 'http://127.0.0.1:4201/api'
+    /* URL : 'http://127.0.0.1:4201/',
+    API_URL : 'http://127.0.0.1:4201/api' */
+    URL : 'http://142.93.223.220:4201/',
+    API_URL : 'http://142.93.223.220:4201/api'
 };
